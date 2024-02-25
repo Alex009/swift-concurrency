512abe8a5b83706bfa66485daad77446dcb8273d
Sergey Panov <spanov@icerockdev.com>
2024-02-25 18:13:07 +0700

    example 5d

---
+++ example5/example5d/race-condition-5d.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+import MD5Hash
+
+var result = [String]()
+
+let start = CFAbsoluteTimeGetCurrent()
+
+func addNewItem() {
+    let hash = generateMD5Hash(strings: result)
+    
+    var newResult = result
+    newResult.append(hash)
+    result = newResult
+}
+
+for _ in 1...1000 {
+    addNewItem()
+}
+
+print("Result: \(result.last ?? "")")
+let diff = CFAbsoluteTimeGetCurrent() - start
+print("Time spent: \(diff) seconds")
+
+RunLoop().run()
